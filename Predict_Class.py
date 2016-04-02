22cd75ed6963838b17cc092b862a5155c75d568e
ZeevSuprun <zeev@inside-87-53.wireless.utoronto.ca>
2016-04-02 15:51:02 -0400

    created function that creates factor tables for joint probability distributions.

--- Predict_Class.py
+++ Predict_Class.py
@@ -2,7 +2,7 @@ from BayesianNetwork import *
 from VariableElimination import *
 
 
-def createFactor (required_var, given_var_list, dataset):
+def create_conditional_factor (required_var, given_var_list, dataset):
     '''
     Given a required variable, a list of given variables, and a dataset.
     Each element in dataset is a line from data.txt.
@@ -59,9 +59,51 @@ def createFactor (required_var, given_var_list, dataset):
     #note: this is already normalized.
     return new_factor
 
-def print_factor(fact):
+def create_variable_factor (var_list, dataset):
+    '''
+    Given a list variables, and a dataset.
+
+    this function will create a probability factor that is a table of values for
+    P(var1, var2, ...)
+    using the dataset to compute this probability.
+    '''
+
+    new_factor = Factor("new_factor", var_list)
+
+    assignments = list(new_factor.get_assignment_iterator())
+
+    examples_with_assignment = [ [] for i in range(len(assignments))]
+    for example in dataset:
+
+        #Determine which assignment of the variables this example corresponds to.
+        this_assignment = []
+        for var in var_list:
+            this_assignment.append( example[int(var.name[0])] )
+
+        #the index of this assignment in the list of assignments.
+        index = assignments.index(this_assignment)
+        examples_with_assignment[index].append(example)
+
+    for index in range(len(assignments)):
+
+        #P(this assignment) = # of examples with this assignment / total.
+        prob = len(examples_with_assignment[index]) / len(dataset)
+
+        new_factor.add_value_at_assignment(prob, assignments[index])
+
+    #note: this is already normalized.
+    return new_factor
+
+
+def print_conditional_factor(fact):
+    #fact is a conditional probability factor table.
     scope = fact.get_scope()
     bckwds_fact = Factor(fact.name, scope[1:] + [scope[0]])
+
+    #backwds_fact is fact, but with the variables in a different order.
+
+    #this is done so that first all outputs with Malignant = 2 are printed, then all outputs with malignant = 4.
+    #this makes things easier to read.
 
     for bckwds_assignment in bckwds_fact.get_assignment_iterator():
         #print(assignment)
@@ -80,6 +122,24 @@ def print_factor(fact):
         out_str += ") = {}".format(fact.get_value(assignment))
         print(out_str)
 
+def print_variable_factor(fact):
+    #fact is a factor table (that isn't conditional probability)
+    scope = fact.get_scope()
+
+    for assignment in fact.get_assignment_iterator():
+        out_str = "P("
+        given_vars = fact.get_scope()
+        cntr = 0
+        for var in given_vars:
+            out_str += var.name[2:] + ' = ' + str(assignment[cntr]) + ', '
+            cntr += 1
+
+        #print(assignment)
+        out_str += ") = {}".format(fact.get_value(assignment))
+        print(out_str)
+
+
+
 def output_to_txt(fact):
     '''
     fact is a factor that must be P(malignant | 1 variable)
@@ -96,8 +156,6 @@ def output_to_txt(fact):
             output_file.write(to_write)
 
 
-
-
 def check_equal(f1, f2):
     '''
     f1 and f2 are 2 factors.
@@ -105,12 +163,19 @@ def check_equal(f1, f2):
     return true if f1 and f2 are equal within an epsilon.
     '''
     are_equal = True
-    epsilon = 0.05
+    epsilon = 0.55
     for asssignment in f1.get_assignment_iterator():
-        diff = f1.get_value(asssignment) - f2.get_value(asssignment)
-        if (not (abs(diff) < epsilon)):
-            are_equal = False
-            break
+        v1 = f1.get_value(asssignment)
+        v2 = f2.get_value(asssignment)
+        #if there was no data on a certain assignment in a training set,
+        # then the value for that assignment is '-' and we ignore it when comparing.
+        if v1 != '-' and v2 != '-':
+
+            diff = v1 - v2
+            #print(abs(diff))
+            if (not (abs(diff) < epsilon)):
+                are_equal = False
+                break
 
     return are_equal
 
@@ -135,7 +200,9 @@ def check_independence(fact1, fact2):
     P(M | B, A) = P(M |A)
     P(M, B | A ) = P(M | A) * P (B | A)
     ----------
-
+    if Pr(B| A^C) = Pr(B|A) then B is conditionally independent of C given A.
+    if Pr(M| A, B) = Pr(M|A) then M is cond. indep of B given A.
+    --------
     To determine if fact1 == fact2, I have 2 options:
 
     1.  write function that calculates P(A, B | C)
@@ -151,14 +218,16 @@ def check_independence(fact1, fact2):
 
     vars_to_check = list(f2_scope.difference(f1_scope))
     #vars to check is a list of variables in f2 but not f1.
-
     var = vars_to_check[0]
 
     are_equal = True
 
     for val in var.domain():
         restricted_f2 = restrict_factor(fact2, var, val)
-
+        #print("factor 1:")
+        #print_factor(fact1)
+        #print("With " + var.name[2:] + " restricted to " + str(val) + ":\n")
+        #print_factor(restricted_f2)
         if(not check_equal(restricted_f2, fact1)):
             are_equal = False
             break
@@ -220,29 +289,48 @@ print(fact.get_scope())
 print_factor(fact)
 '''
 
-#fact1a = P(Malignant | Uniformity of Cell Size)
+
+'''
+#generate a list of factors, P(Malignant | Var) for all variables
 factor_list = []
 for var in var_list:
     if var != var_list[-1]:
         factor_list.append(createFactor(var_list[-1], [var], training_data))
         output_to_txt(factor_list[-1])
+'''
 
-#fact1a = createFactor(var_list[-1], [var_list[1]], training_data)
+'''
+#check for conditional independence for all possible variable combinations.
+for var1 in var_list:
+    for var2 in var_list:
+        if var1 != var_list[-1] and var2 != var_list[-1] and var1 != var2:
+            #fact1 is P(malignancy | varA). fact2 is P(malignancy | varA, varB)
+            fact1 = createFactor(var_list[-1], [var1], training_data)
+            fact2 = createFactor(var_list[-1], [var1, var2], training_data)
+            indep = check_independence(fact1, fact2)
+            if indep:
+                #if Pr(M| A, B) = Pr(M|A) then M is cond. indep of B given A.
+                print("malignancy is conditionally independent of " + var2.name[2:] + " given " + var1.name[2:])
+            else:
+                print("no independence found for these variables")
+'''
 
+# if epsilon = 0.55, then
+# malignancy is conditionally independent of Uniformity of Cell Shape given Uniformity of Cell Size
+#but no other conditional independence with smaller epsilon.
 
-#fact1b = P(Malignant | Clump_thickness)
-#fact1b = createFactor(var_list[-1], [var_list[0]], training_data)
-
+#fact1 = P(Malignant | Clump_thickness)
+fact1 = create_conditional_factor(var_list[-1], [var_list[0]], training_data)
 
 #fact2 is P(Malignant | Clump Thickness, Uniformity of Cell Size)
-#fact2 = createFactor(var_list[-1], [var_list[0], var_list[1]], training_data)
-#print_factor(fact1b)
+fact2 = create_conditional_factor(var_list[-1], [var_list[0], var_list[1]], training_data)
+#print_factor(fact1)
 #print_factor(fact2)
 
-#print(check_independence(fact1b, fact2))
 
-
-
+#print(check_independence(fact1, fact2))
+prob_var = create_variable_factor([var_list[1], var_list[-1]], training_data)
+print_variable_factor(prob_var)
 
 
 
