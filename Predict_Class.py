85a40543203f7929de94897a07c52b56e270857a
ZeevSuprun <zeev@inside-87-210.wireless.utoronto.ca>
2016-04-07 02:06:10 -0400

    Small changes to get code ready for submission

--- Predict_Class.py
+++ Predict_Class.py
@@ -52,6 +52,7 @@ def create_conditional_factor (required_var, given_var_list, dataset):
             if (num_examples != 0):
                 prob = num_each_value[val] / num_examples
             else:
+                #no examples available to get data about this value combination, output '-' as probability.
                 prob = "-"
 
             new_factor.add_value_at_assignment(prob, [val] + assignments[index])
@@ -124,7 +125,7 @@ def print_conditional_factor(fact):
         print(out_str)
 
 def print_variable_factor(fact):
-    #fact is a factor table (that isn't conditional probability)
+    #fact is a factor table (that isn't a conditional probability)
     scope = fact.get_scope()
 
     for assignment in fact.get_assignment_iterator():
@@ -174,7 +175,7 @@ def output_joint_distrib_to_txt(fact, filename):
 def check_equal(f1, f2):
     '''
     f1 and f2 are 2 factors.
-    They should have the same scope, in the same order. (? I think so, at least?)
+    They should have the same scope, in the same order.
     return true if f1 and f2 are equal within an epsilon.
     '''
     are_equal = True
@@ -199,32 +200,12 @@ def check_independence(fact1, fact2):
     fact1 is P(malignancy | varA)
     fact2 is P(malignancy | varA, varB)
 
-    Does fact1 == fact2?
-    If you assume fact1 == fact2, what error do you get?
-
-    if P(M|A) = P(M|A,B) then:
-
-    -------
-    R, B are cond. indep. given Y iff
-    P(R, B | Y ) = P(R | Y) * P (B | Y)
-    this is equivalent to:
-    P(R | B, Y) = P(R |Y)
-
-    ----------
-    M, cond. indep. of B given A iff
-    P(M | B, A) = P(M |A)
-    P(M, B | A ) = P(M | A) * P (B | A)
-    ----------
-    if Pr(B| A^C) = Pr(B|A) then B is conditionally independent of C given A.
     if Pr(M| A, B) = Pr(M|A) then M is cond. indep of B given A.
-    --------
-    To determine if fact1 == fact2, I have 2 options:
+    This function checks if fact1 == fact2.
 
-    1.  write function that calculates P(A, B | C)
-        then I can calculate P(M, B | A), and multiply P(M | A) by P(B | A) and check if they are equal (within some epsilon)
-        if so, M, B are indep given A.
-
-    2.  For all B = b, check if P(M | B = b, A) = P(M | A), cause if so they are cond indep.
+    To determine if fact1 == fact2,
+    For all B = b, check if P(M | B = b, A) = P(M | A),
+    if so, M is conditionally independent of B given A, and return true.
 
     if cond indep return true.
     '''
@@ -239,10 +220,7 @@ def check_independence(fact1, fact2):
 
     for val in var.domain():
         restricted_f2 = restrict_factor(fact2, var, val)
-        #print("factor 1:")
-        #print_factor(fact1)
-        #print("With " + var.name[2:] + " restricted to " + str(val) + ":\n")
-        #print_factor(restricted_f2)
+
         if(not check_equal(restricted_f2, fact1)):
             are_equal = False
             break
@@ -257,10 +235,10 @@ def find_correlation(varX, varY, dataset):
 
     sum_x = 0
     sum_y = 0
-    sum_xy = 0
+    sum_xy = 0 #sum (x*y)
 
     sum_x2 = 0 # sum (x^2)
-    sum_y2 = 0
+    sum_y2 = 0 # sum (y^2)
 
     x_index = int(varX.name[0])
     y_index = int(varY.name[0])
@@ -275,6 +253,7 @@ def find_correlation(varX, varY, dataset):
         sum_x2 += x_i**2
         sum_y2 += y_i**2
 
+    #formula for correlation between x and y.
     corr = (n * sum_xy - sum_x * sum_y) / ((n*sum_x2 - sum_x**2)**0.5 * (n*sum_y2 - sum_y**2)**0.5)
     return corr
 
@@ -283,7 +262,9 @@ def naive_bayes_predict(class_var, var_list, training_data, test_data):
     class_var is the "class" variable that can take a value of 2 for benign and 4 for malignant.
     var_list is a list of variables (not including the class_var)
     training_data is an array of examples from data.txt to be used for training.
-    test_data is another array of examples from data.txt.
+    test_data is another array of examples from data.txt to be used for testing.
+
+    This function prints the F1 score and returns the classification rate on test_data.
 
     predicted class = argmax over v ( P(Y = v) * Product(P(Xi = ui | Y = v) )
     '''
@@ -301,7 +282,6 @@ def naive_bayes_predict(class_var, var_list, training_data, test_data):
     false_neg = 0
     #for every example in the test_data, predict the outcome.
     for example in test_data:
-
         #populate vals_to_assign using the example
         for i in range(len(vals_to_assign)):
             vals_to_assign[i] = example[int(var_list[i].name[0])]
@@ -332,20 +312,19 @@ def naive_bayes_predict(class_var, var_list, training_data, test_data):
         elif (predicted_result == 2 and actual_class == 4):
             false_neg += 1
 
-    print("number of true positives is {}".format(true_pos))
-    print("number of false positives is {}".format(false_pos))
-    print("number of false negatives is {}".format(false_neg))
+    #print("number of true positives is {}".format(true_pos))
+    #print("number of false positives is {}".format(false_pos))
+    #print("number of false negatives is {}".format(false_neg))
 
     precision = true_pos / (true_pos + false_pos)
     recall = true_pos / (true_pos + false_neg)
 
-    print("Precision is {}".format(precision))
-    print("Recall is {}".format(recall))
+    #print("Precision is {}".format(precision))
+    #print("Recall is {}".format(recall))
 
     F1 = 2*precision*recall / (precision + recall)
     print("F1 score is {}".format(F1))
     #return the rate of correct predictions.
-    print(len(test_data))
     return num_correct_predictions / len(test_data)
 
 
@@ -395,33 +374,25 @@ for line in data_file:
             split_line[i] = int(split_line[i])
         training_data.append(split_line[1:])
 
-'''
-#fact = createFactor(var_list[-1], [var_list[0], var_list[1]], training_data)
-fact = createFactor(var_list[-1], [var_list[1]], training_data)
-print(fact)
-print(fact.get_scope())
-print_factor(fact)
-'''
-
 
 '''
-#generate a list of factors, P(Malignant | Var) for all variables
+#generate a list of factors, P(Malignant | Var) for all variables, and output to a .txt file.
 factor_list = []
 for var in var_list:
     if var != var_list[-1]:
-        factor_list.append(createFactor(var_list[-1], [var], training_data))
+        factor_list.append(create_conditional_factor(var_list[-1], [var], training_data))
         output_to_txt(factor_list[-1])
-'''
 
 
-#generate a list of joint probability factors, P(Malignant, Var) for all variables
+#generate a list of joint probability factors, P(Malignant, Var) for all variables, and output to a .txt file.
 factor_list = []
 for var in var_list:
     if var != var_list[-1]:
         factor_list.append(create_variable_factor([var, var_list[-1]], training_data))
         output_joint_distrib_to_txt(factor_list[-1], "joint_pdf" + str(var_list.index(var)))
 
-
+#The code in this comment block was used to output files, which were then used to create informative graphs.
+'''
 
 '''
 #check for conditional independence for all possible variable combinations.
@@ -429,34 +400,21 @@ for var1 in var_list:
     for var2 in var_list:
         if var1 != var_list[-1] and var2 != var_list[-1] and var1 != var2:
             #fact1 is P(malignancy | varA). fact2 is P(malignancy | varA, varB)
-            fact1 = createFactor(var_list[-1], [var1], training_data)
-            fact2 = createFactor(var_list[-1], [var1, var2], training_data)
+            fact1 = create_conditional_factor(var_list[-1], [var1], training_data)
+            fact2 = create_conditional_factor(var_list[-1], [var1, var2], training_data)
             indep = check_independence(fact1, fact2)
             if indep:
                 #if Pr(M| A, B) = Pr(M|A) then M is cond. indep of B given A.
                 print("malignancy is conditionally independent of " + var2.name[2:] + " given " + var1.name[2:])
             else:
                 print("no independence found for these variables")
+
+#Running the code in this comment block found no conditional independence in the data. This information was used when
+#choosing the bayes net structure.
 '''
 
-# if epsilon = 0.55, then
-# malignancy is conditionally independent of Uniformity of Cell Shape given Uniformity of Cell Size
-#but no other conditional independence with smaller epsilon.
-
-#fact1 = P(Malignant | Clump_thickness)
-fact1 = create_conditional_factor(var_list[-1], [var_list[-2]], training_data)
-#print_conditional_factor(fact1)
-
-#fact2 is P(Malignant | Clump Thickness, Uniformity of Cell Size)
-fact2 = create_conditional_factor(var_list[-1], [var_list[0], var_list[1]], training_data)
-#print_factor(fact1)
-#print_factor(fact2)
-
-#print(check_independence(fact1, fact2))
-prob_var = create_variable_factor([var_list[-2], var_list[-1]], training_data)
-#print_variable_factor(prob_var)
-
 '''
+#Printing the correlation matrix
 correlation_matrix = [[0.0 for i in var_list] for j in var_list]
 print(names)
 for i in range(len(var_list)):
@@ -484,9 +442,11 @@ print(check_equal(fact_join, mult_fact))
 # It turns out they are equal up to an epsilon of 0.1 for these two variables. (mitosis and class)
 '''
 
-#prediction_rate = naive_bayes_predict(var_list[-1], var_list[0:len(var_list) - 1], training_data, training_data)
-n = 200
-prediction_rate = naive_bayes_predict(var_list[-1], var_list[0:len(var_list) - 1], training_data[0:n], training_data[n:367])
+#names = ["Clump Thickness", "Uniformity of Cell Size", "Uniformity of Cell Shape", "Marginal Adhesion","Single Epithelial Cell Size", "Bare Nuclei", "Bland Chromatin", "Normal Nucleoli", "Mitoses", "Class"]
 
+n = 478
 
+best_vars = [var_list[1], var_list[4], var_list[6]]
+#prediction_rate = naive_bayes_predict(var_list[-1], var_list[0:len(var_list) - 2], training_data[0:n], training_data[n:])
+prediction_rate = naive_bayes_predict(var_list[-1], best_vars, training_data[0:n], training_data[n:])
 print(prediction_rate)
